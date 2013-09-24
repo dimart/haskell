e17e4a74485eccab1b520f6767a17021af78c4a2
Dmitriy <dimart.sp@gmail.com>
2013-09-24 15:09:11 +0400

    Update HomeWork3.hs

--- HomeWork3.hs
+++ HomeWork3.hs
@@ -4,7 +4,7 @@
 module HomeWork3 where
 
 fib = 1 : 1 : [ a + b | (a,b) <- zip' fib (tail fib)] where   
-							   zip' (x:xs) (y:ys) = (x,y) : zip xs ys
-							   zip' []     []     = []
+				 zip' (x:xs) (y:ys) = (x,y) : zip xs ys
+				 zip' []     []     = []
 
 primes = 2 : pr [3,5..] where pr (x:xs) = x : (pr $ filter (\y -> y `mod` x /= 0) xs) 
